d23fc43235e83a74e6ea54a847c1cb97c024c370
Arjun Rai <raiarjun9415@gmail.com>
2025-06-15 01:03:36 +0530

    Chat Routes

---
+++ backend/controllers/chat.controller.js
@@ -0,0 +1,10 @@
+export async function getStreamToken(req, res) {
+    try {
+        const userId = req.user.id;
+        const token = await generateStreamToken(userId);
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error("Error in getStreamToken controller", error.message);
+        return res.status(500).json({ message: "Internal Server error" });
+    }   
+}
