2c2b865fafcfa24079f8850f629f96f9ac3e8ba4
Arjun Rai <raiarjun9415@gmail.com>
2025-06-16 03:42:42 +0530

    Created useLogin hook

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,15 @@
+import { login } from "../lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+
+const useLogin = () => {
+
+    const queryClient=useQueryClient();
+
+    const {mutate:loginMutation,isPending,error}=useMutation({
+        mutationFn:login,
+        onSuccess:()=>queryClient.invalidateQueries({queryKey:["authUser"]}),
+    });
+    return {loginMutation,isPending,error};
+}
+
+export default useLogin
